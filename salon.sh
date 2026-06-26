53acece51e2deaad5e260da292fb0e6636e417b1
vitalid74 <138309965+vitalid74@users.noreply.github.com>
2026-06-26 03:14:04 +0300

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -19,8 +19,8 @@ MAIN_MENU() {
     do
       echo "$SERVICE_ID) $SERVICE_NAME"
     done
-    read SERVICE_ID_INPUT
-    SERVICE_ID=$($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID_INPUT;")
+    read SERVICE_ID_SELECTED
+    SERVICE_ID=$($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID_SELECTED;")
     if [[ -z $SERVICE_ID ]]
     then 
       MAIN_MENU "I could not find that service. What would you like today?"
@@ -32,34 +32,34 @@ MAIN_MENU() {
 
 GET_USER () {
     echo -e "\nWhat's your phone number?"
-    read PHONE_NUMBER
+    read CUSTOMER_PHONE
 
-    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER';")
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE';")
     if [[ -z $CUSTOMER_ID ]]
     then
       echo -e "\nI don't have a record for that phone number, what's your name?"
       read CUSTOMER_NAME
 
-      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES ('$PHONE_NUMBER','$CUSTOMER_NAME');")
-      if [[ INSERT_CUSTOMER_RESULT != "INSERT 0 1" ]]
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES ('$CUSTOMER_PHONE','$CUSTOMER_NAME');")
+      if [[ $INSERT_CUSTOMER_RESULT != "INSERT 0 1" ]]
       then 
         echo "Error adding customer"
       fi
-      CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER';")
+      CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE';")
     fi
-    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE customer_id = CUSTOMER_ID;")
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE customer_id = $CUSTOMER_ID;")
     MAKE_RESERVATION
 }
 
 MAKE_RESERVATION() {
   SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID;")
   echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
-  read TIME_INPUT
+  read SERVICE_TIME
 
-  INSERT_APPOINTMENT_RESULT="$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ($CUSTOMER_ID,$SERVICE_ID,'$TIME_INPUT');")"
+  INSERT_APPOINTMENT_RESULT="$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ($CUSTOMER_ID,$SERVICE_ID,'$SERVICE_TIME');")"
   if [[ $INSERT_APPOINTMENT_RESULT = "INSERT 0 1" ]]
   then
-    echo -e "\nI have put you down for a $SERVICE_NAME at $TIME_INPUT, $CUSTOMER_NAME."
+    echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
   fi
 }
 
